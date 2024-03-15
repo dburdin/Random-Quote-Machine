4c8a0f88876a250f9f0c355d9acc4773ce7548a1
Dmytrii Bevziuk <113050865+dburdin@users.noreply.github.com>
2024-03-15 20:58:40 +0200

    adding json file as data

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,26 @@
+import data from "../data/data.json";
+
 export const App = () => {
+  console.log(data);
+
   return (
-    <>
-      <div>Qoute</div>
-    </>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div id="quote-box">
+        <p id="text">Quote</p>
+        <p id="author">Author</p>
+        <button id="new-quote">Add new quote</button>
+        <a href="twitter.com/intent/tweet" id="tweet-quote">
+          Twitter
+        </a>
+      </div>
+    </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { App } from "./components/App.tsx";
 
 import "./styles/reset.css";
+import "./styles/general.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
